6f5f1e9639c2fbf69605975cf92b0c0dc002511e
jincrates <94jingyu@gmail.com>
2021-10-04 16:05:48 +0900

    HelloWorldController 추가

---
+++ src/main/java/com/jincrates/restfulwebservice/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.jincrates.restfulwebservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    // GET
+    // /hello-world (endpoint)
+    // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+
+}
