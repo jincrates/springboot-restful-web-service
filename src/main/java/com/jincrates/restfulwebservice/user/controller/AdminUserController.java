85ba464c60d05394522cb6447f236a0b21bef9d9
jincrates <94jingyu@gmail.com>
2021-10-10 18:50:40 +0900

    REST API 버전 관리 1. URL를 통한 버전 관리 2. Parameter를 통한 버전 관리 3. Header를 통환 버전 관리 4. Mime-Type을 통한 버전 관리

--- src/main/java/com/jincrates/restfulwebservice/user/controller/AdminUserController.java
+++ src/main/java/com/jincrates/restfulwebservice/user/controller/AdminUserController.java
@@ -41,7 +41,7 @@ public class AdminUserController {
         return customFilter(filterSet, "UserInfo", users);
     }
 
-    // GET /admin/users/1 -> /admin/v1/users/1
+    // 1. URI를 이용한 API Version 관리
     @GetMapping("/v1/users/{id}")
     public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = service.findOne(id);
@@ -61,6 +61,129 @@ public class AdminUserController {
 
     @GetMapping("/v2/users/{id}")
     public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // User -> UserV2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2); // id, name, joinDate, password, ssm
+        userV2.setGrade("VIP");
+
+        Set<String> filterSet = new HashSet<String>();
+        filterSet.add("id");
+        filterSet.add("name");
+        filterSet.add("joinDate");
+        filterSet.add("grade");
+
+        return customFilter(filterSet, "UserInfoV2", userV2);
+    }
+
+
+    // 2. Parameter를 이용한 API Version 관리
+    @GetMapping(value = "/users/{id}/", params = "version=1")
+    public MappingJacksonValue retrieveUserParamV1(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        Set<String> filterSet = new HashSet<String>();
+        filterSet.add("id");
+        filterSet.add("name");
+        filterSet.add("joinDate");
+        filterSet.add("ssn");
+
+        return customFilter(filterSet, "UserInfo", user);
+    }
+
+    @GetMapping(value = "/users/{id}/", params = "version=2")
+    public MappingJacksonValue retrieveUserParamV2(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // User -> UserV2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2); // id, name, joinDate, password, ssm
+        userV2.setGrade("VIP");
+
+        Set<String> filterSet = new HashSet<String>();
+        filterSet.add("id");
+        filterSet.add("name");
+        filterSet.add("joinDate");
+        filterSet.add("grade");
+
+        return customFilter(filterSet, "UserInfoV2", userV2);
+    }
+
+
+    // 3. Header를 이용한 API Version 관리
+    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1")  //임의값 입력
+    public MappingJacksonValue retrieveUserHeaderV1(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        Set<String> filterSet = new HashSet<String>();
+        filterSet.add("id");
+        filterSet.add("name");
+        filterSet.add("joinDate");
+        filterSet.add("ssn");
+
+        return customFilter(filterSet, "UserInfo", user);
+    }
+
+    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")
+    public MappingJacksonValue retrieveUserHeaderV2(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // User -> UserV2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2); // id, name, joinDate, password, ssm
+        userV2.setGrade("VIP");
+
+        Set<String> filterSet = new HashSet<String>();
+        filterSet.add("id");
+        filterSet.add("name");
+        filterSet.add("joinDate");
+        filterSet.add("grade");
+
+        return customFilter(filterSet, "UserInfoV2", userV2);
+    }
+
+
+    // 4. Mime-Type을 이용한 API Version 관리
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json")
+    public MappingJacksonValue retrieveUserMimeV1(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        Set<String> filterSet = new HashSet<String>();
+        filterSet.add("id");
+        filterSet.add("name");
+        filterSet.add("joinDate");
+        filterSet.add("ssn");
+
+        return customFilter(filterSet, "UserInfo", user);
+    }
+
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv2+json")
+    public MappingJacksonValue retrieveUserMimeV2(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
