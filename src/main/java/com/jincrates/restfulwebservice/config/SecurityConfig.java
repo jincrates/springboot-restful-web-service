1e2cf20e368c775bbb23f8d29c159c0cc813f598
jincrates <94jingyu@gmail.com>
2021-10-21 23:06:05 +0900

    JPA를 사용을 위한 Dependency 추가와 설정

--- src/main/java/com/jincrates/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/jincrates/restfulwebservice/config/SecurityConfig.java
@@ -3,10 +3,18 @@ package com.jincrates.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configurable
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
